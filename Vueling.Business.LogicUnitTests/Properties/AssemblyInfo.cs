81feba2b5a9a63707d04ee3d03a86bf973c45cd3
Carlos Sanchez Romero <carles.sanchez.romero@gmail.com>
2018-04-10 19:50:36 +0200

    Añade Test Unitarios de la capa Business

---
+++ Vueling.Business.LogicUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Vueling.Businmess.LogicUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Vueling.Businmess.LogicUnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("87f4ccd2-9598-49be-b1ab-aa40acd4529f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Vueling.Business.LogicUnitTests/StudentBLTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NMock;
+using Vueling.Common.Logic.Model;
+using Vueling.DataAccess.DAO;
+
+namespace Vueling.Business.LogicUnitTests
+{
+    [TestClass]
+    public class StudentBLTests
+    {
+        private MockFactory _mockFactory;
+        private Mock<ISelectDAO<Student>> _crudReadStudent;
+        private Mock<IInsertDAO<Student>> _crudSaveStudent;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _mockFactory = new MockFactory();
+            _crudReadStudent = _mockFactory.CreateMock<ISelectDAO<Student>>();
+            _crudSaveStudent = _mockFactory.CreateMock<IInsertDAO<Student>>();
+        }
+
+
+        [TestMethod]
+        public void CrudSaveAddTestBL()
+        {
+            var guidTest = Guid.NewGuid();
+            var studentTest = new Student
+            {
+                Guid = guidTest,
+                ID = 1,
+                Nombre = "Carlos",
+                Apellidos = "Sanchez Romero",
+                DNI = "44445555T",
+                FechaDeNacimiento = DateTime.Parse("28/02/1988"),
+                Edad = 30
+            };
+            var studentReturnStub = (Student)studentTest.Clone();
+            _crudSaveStudent.Expects.One.MethodWith(sm => sm.Add(studentTest)).WillReturn(studentReturnStub);
+
+            var studentReturn = _crudSaveStudent.MockObject.Add(studentTest);
+
+            Assert.IsTrue(studentTest.Equals(studentReturn));
+           
+        }
+    }
+}
