53ec1f0e85794fbb12ab20ea1c5dff15c1897fc1
Carles Sanchez Romero <carles.sanchez@atmira.com>
2018-04-16 21:35:07 +0200

    Empieza a implementar BDD

---
+++ Vueling.Business.LogicBDDTests/StudentBLCalcYearsOld.feature
@@ -0,0 +1,9 @@
+﻿Feature: StudentBLCalcYearsOld
+	I need to calculate the years between 2 Dates
+
+@mytag
+Scenario: Calcualte Years Old
+	Given I have entered "16/04/2018" into the Year calculator
+	And I have also entered "28/02/1988" into the calculator
+	When I call CalcEdad
+	Then the result should be 30 on the screen
---
+++ Vueling.Business.LogicBDDTests/StudentBLCalcYearsOldSteps.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TechTalk.SpecFlow;
+using Vueling.Business.Logic;
+
+namespace Vueling.Business.LogicBDDTests
+{
+    [Binding]
+    public class StudentBLCalcYearsOldSteps
+    {
+
+        private DateTime _fechaNacimiento;
+        private DateTime _fechaRegistro;
+        private int _edad;
+        private IStudentBL _studentBL = new StudentBL();
+
+        [Given(@"I have entered ""(.*)"" into the Year calculator")]
+        public void GivenIHaveEnteredIntoTheYearCalculator(string fechaRegistro)
+        {
+            _fechaRegistro = DateTime.Parse(fechaRegistro);
+        }
+        
+        [Given(@"I have also entered ""(.*)"" into the calculator")]
+        public void GivenIHaveAlsoEnteredIntoTheCalculator(string fechaNacimiento)
+        {
+            _fechaNacimiento = DateTime.Parse(fechaNacimiento);
+        }
+
+        [When(@"I call CalcEdad")]
+        public void WhenICallCalcEdad()
+        {
+            _edad = _studentBL.CalcularEdad(_fechaRegistro, _fechaNacimiento);
+        }
+        
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int result)
+        {
+            Assert.AreEqual(_edad, result);
+        }
+    }
+}
